98261c8ac04f5639e2f05bce3ad05f0744f6c763
rraphael <raphael.rameix@ynov.com>
2025-10-07 09:11:30 +0200

    Créé la grille et place les jetons

--- Grille.go
+++ Grille.go
@@ -4,24 +4,28 @@ import (
 	"fmt"
 )
 
-func Init_Grille(){
+func Init_Grille(Grille *[6][7]int){
 
-	for x := 0 ; x < 7; x++ {
-        for y := 0; y < 6; y++ {
-            *Grille[x][y] = 0
+	for x := 0 ; x < 6; x++ {
+        for y := 0; y < 7; y++ {
+            Grille[x][y] = 0
 		}
 	}
 	fmt.Print(*Grille)
 
 }
 
-func Grille_Jeton(y int, nbjoueur int){
+func Grille_Jeton(y int, nbjoueur int, Grille *[6][7]int) string{
+	if y < 0 || y >= 7 {
+        return "Erreur: colonne invalide"
+    }
+    
 	for x := 5; x > 0; x--{
-		if *Grille[y][x] != 0 {
-			break
-		} else {
-			*Grille[x][y] = nbjoueur
+		if Grille[y][x] == 0 {
+			Grille[x][y] = nbjoueur
+			return "Jeton placé avec succès"
 		}
 	}
-	return *Grille
+	fmt.Print(Grille)
+	return "Erreur: colonne pleine"
 }
