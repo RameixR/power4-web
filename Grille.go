016807378f82ab5723b53ab05aa6fbb8666d84fa
Hugo-Lourenco <hugo.lourenco@ynov.com>
2025-10-14 11:08:03 +0200

    test

--- Grille.go
+++ Grille.go
@@ -1,31 +1,26 @@
 package power4
 
-import (
-	"fmt"
-)
-
-func Init_Grille(Grille *[6][7]int){
-
-	for x := 0 ; x < 6; x++ {
+func Init_Grille(Grille *[6][7]int) {
+    for x := 0; x < 6; x++ {
         for y := 0; y < 7; y++ {
             Grille[x][y] = 0
-		}
-	}
-	fmt.Print(*Grille)
-
+        }
+    }
 }
 
-func Grille_Jeton(y int, nbjoueur int, Grille *[6][7]int) string{
-	if y < 0 || y >= 7 {
-        return "Erreur: colonne invalide"
+func Grille_Jeton(y int, nbjoueur int, Grille *[6][7]int) string {
+    row, ok := DropToken(Grille, y, nbjoueur)
+    if !ok {
+        if y < 0 || y >= 7 {
+            return "Erreur: colonne invalide"
+        }
+        return "Erreur: colonne pleine"
     }
-    
-	for x := 5; x >= 0; x--{
-		if Grille[x][y] == 0 {
-			Grille[x][y] = nbjoueur
-			return "Jeton placé avec succès"
-		}
-	}
-	fmt.Print(Grille)
-	return "Erreur: colonne pleine"
+    if CheckWin(Grille, row, y, nbjoueur) {
+        return "Jeton placé: victoire"
+    }
+    if IsDraw(Grille) {
+        return "Jeton placé: match nul"
+    }
+    return "Jeton placé avec succès"
 }
